79b52b84b7082091a8d5eff370bc4c05bbb9721e
Dhanushk52 <djdhanush52@gmail.com>
2025-08-04 15:27:57 +0530

    Update board_led.h

--- Custom_Drivers/LED_DRIV/board_led.h
+++ Custom_Drivers/LED_DRIV/board_led.h
@@ -1,6 +1,9 @@
 #ifndef BOARD_LED_H
 #define BOARD_LED_H
 
+#define BOARD_LED_PORT     GPIOC
+#define BOARD_LED_PIN      GPIO_PIN_13
+
 void board_led_init(void);
 void board_led_On(void);
 void board_led_Off(void);
